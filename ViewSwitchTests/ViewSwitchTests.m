b3a98fbf0f8349978ccb89c85e7c0a9917791f47
yiyucanglang <zhouyijin@zhouyijindeMacBook-Pro-10.local>
2014-08-16 20:11:27 +0800

    Initial Commit

---
+++ ViewSwitchTests/ViewSwitchTests.m
@@ -0,0 +1,34 @@
+//
+//  ViewSwitchTests.m
+//  ViewSwitchTests
+//
+//  Created by 周义进 on 14-8-16.
+//  Copyright (c) 2014年 苏州达芯微. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ViewSwitchTests : XCTestCase
+
+@end
+
+@implementation ViewSwitchTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
